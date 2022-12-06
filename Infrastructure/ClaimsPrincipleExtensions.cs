4cea1821825feca8b91cec85ecf8b9903001e90a
Deyan Diulgerov <Chukstv@abv.bg>
2022-12-06 18:14:45 +0200

    Introduced ClaimsPrincipceExtensions for User Id

--- Infrastructure/ClaimsPrincipleExtensions.cs
+++ Infrastructure/ClaimsPrincipleExtensions.cs
@@ -5,6 +5,6 @@ namespace WebProject.Infrastructure
     public static class ClaimsPrincipleExtensions
     {
         public static string Id(this ClaimsPrincipal user)
-            => user.FindFirst(ClaimTypes.NameIdentifier).Value;
+            => user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
     }
 }
