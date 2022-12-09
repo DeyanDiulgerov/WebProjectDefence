73ca6b6fc4652dd2029be1a7f6f33ad9208a79a6
Deyan Diulgerov <Chukstv@abv.bg>
2022-12-09 13:23:42 +0200

    gameService Unit Tests and HomeController Error View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WebProject.Models;
 
 namespace WebProject.Controllers
 {
@@ -8,6 +9,12 @@ namespace WebProject.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
